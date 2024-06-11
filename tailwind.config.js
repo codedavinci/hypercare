5946e7cbb71b94b7b6a50b87607f2901e95a99de
Eddie Silva <codedavinci.now@gmail.com>
2024-06-11 15:54:29 -0400

    chore(Styles): Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  daisyui: {
+    themes: ['light', 'winter', 'night']
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/line-clamp')
+  ],
+}
+
